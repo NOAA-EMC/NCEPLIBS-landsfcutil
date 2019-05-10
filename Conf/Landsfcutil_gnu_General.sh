03b7d06268dcdc9c6354b89d5fd372cab97fe74a
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-05-10 20:14:54 +0000

    Unified build (20190510) script/makefile landsfcutil

--- Conf/Landsfcutil_gnu_General.sh
+++ Conf/Landsfcutil_gnu_General.sh
@@ -21,10 +21,12 @@
  export MPIFC=mpigfortran
 
  export DEBUG="-g -O0"
- export CFLAGS="-O3 -DUNDERSCORE -DLINUX -fPIC"
+ export CFLAGS="-O3 -fPIC"
  export FFLAGS="-O3 -fPIC"
+ export FREEFORM="-ffree-form"
+ export FPPCPP="-cpp"
  export CPPFLAGS="-P -traditional-cpp"
- export MPICFLAGS="-O3 -DUNDERSCORE -DLINUX -fPIC"
+ export MPICFLAGS="-O3 -fPIC"
  export MPIFFLAGS="-O3 -fPIC"
  export MODPATH="-J"
  export I4R4=""
@@ -32,8 +34,8 @@
  export I8R8="-fdefault-integer-8 -fdefault-real-8"
 
  export CPPDEFS=""
- export CFLAGSDEFS=""
- export FFLAGSDEFS="-ffree-form"
+ export CFLAGSDEFS="-DUNDERSCORE -DLINUX"
+ export FFLAGSDEFS=""
 
  export USECC=""
  export USEFC="YES"
