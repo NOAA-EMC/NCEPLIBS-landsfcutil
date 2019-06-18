2539614f21937b9eb2adc02f990c9771d3eb4797
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-06-18 15:21:45 +0000

    Unified build (20190618) script/makefile landsfcutil

--- build_landsfcutil.sh
+++ build_landsfcutil.sh
@@ -1,37 +1,6 @@
 #!/bin/sh
 
- (( $# == 0 )) && {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
-
- sys=${1,,}
- [[ $sys == wcoss || $sys == dell || $sys == cray ||\
-    $sys == theia || $sys == intel_general || $sys == gnu_general ]] || {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
- debg=false
- inst=false
- skip=false
- local=false
- (( $# > 1 )) && {
-   [[ ${2,,} == build ]] && debg=false
-   [[ ${2,,} == debug ]] && debg=true
-   [[ ${2,,} == install ]] && inst=true
-   [[ ${2,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${2,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${2,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
- (( $# > 2 )) && {
-   [[ ${3,,} == build ]] && debg=false
-   [[ ${3,,} == debug ]] && debg=true
-   [[ ${3,,} == install ]] && inst=true
-   [[ ${3,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${3,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${3,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
-
+ source ./Conf/Analyse_args.sh
  source ./Conf/Collect_info.sh
  source ./Conf/Gen_cfunction.sh
  source ./Conf/Reset_version.sh
@@ -104,27 +73,38 @@ set -x
 #     Install libraries and source files 
 #
    $local && {
-              LIB_DIR4=..
-              LIB_DIRd=..
-              INCP_DIR=../include
-              [ -d $INCP_DIR ] || { mkdir -p $INCP_DIR; }
-              INCP_DIR4=$INCP_DIR
-              INCP_DIRd=$INCP_DIR
-              SRC_DIR=
-             } || {
-              LIB_DIR4=$(dirname $LANDSFCUTIL_LIB4)
-              LIB_DIRd=$(dirname $LANDSFCUTIL_LIBd)
-              INCP_DIR4=$(dirname $LANDSFCUTIL_INC4)
-              INCP_DIRd=$(dirname $LANDSFCUTIL_INCd)
-              SRC_DIR=$LANDSFCUTIL_SRC
-              [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
-              [ -d $LIB_DIRd ] || mkdir -p $LIB_DIRd
-              [ -d $LANDSFCUTIL_INC4 ] && { rm -rf $LANDSFCUTIL_INC4; } \
-                                       || { mkdir -p $INCP_DIR4; }
-              [ -d $LANDSFCUTIL_INCd ] && { rm -rf $LANDSFCUTIL_INCd; } \
-                                       || { mkdir -p $INCP_DIRd; }
-              [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
-             }
+     instloc=..
+     LIB_DIR4=$instloc
+     LIB_DIRd=$instloc
+     INCP_DIR=$instloc/include
+     [ -d $INCP_DIR ] || { mkdir -p $INCP_DIR; }
+     INCP_DIR4=$INCP_DIR
+     INCP_DIRd=$INCP_DIR
+     SRC_DIR=
+   } || {
+     [[ $instloc == --- ]] && {
+       LIB_DIR4=$(dirname $LANDSFCUTIL_LIB4)
+       LIB_DIRd=$(dirname $LANDSFCUTIL_LIBd)
+       INCP_DIR4=$(dirname $LANDSFCUTIL_INC4)
+       INCP_DIRd=$(dirname $LANDSFCUTIL_INCd)
+       SRC_DIR=$LANDSFCUTIL_SRC
+     } || {
+       LIB_DIR4=$instloc
+       LIB_DIRd=$instloc
+       INCP_DIR=$instloc/include
+       INCP_DIR4=$INCP_DIR
+       INCP_DIRd=$INCP_DIR
+       SRC_DIR=$instloc/src
+       [[ $instloc == .. ]] && SRC_DIR=
+     }
+     [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
+     [ -d $LIB_DIRd ] || mkdir -p $LIB_DIRd
+     [ -d $LANDSFCUTIL_INC4 ] && { rm -rf $LANDSFCUTIL_INC4; } \
+                              || { mkdir -p $INCP_DIR4; }
+     [ -d $LANDSFCUTIL_INCd ] && { rm -rf $LANDSFCUTIL_INCd; } \
+                              || { mkdir -p $INCP_DIRd; }
+     [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
+   }
 
    make clean LIB=
    make install LIB=$landsfcutilLib4 MOD=$landsfcutilInc4 \
