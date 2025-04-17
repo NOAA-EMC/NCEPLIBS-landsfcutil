b3ac7275900b8100449cab7ab414503e87c0d771
Alex Richert <alexander.richert@noaa.gov>
2025-04-17 15:23:56 -0700

    Update package.py

--- spack/package.py
+++ spack/package.py
@@ -47,5 +47,5 @@ class Landsfcutil(CMakePackage):
         return (None, None, flags)
 
     def check(self):
-        with working_dir(self.builder.build_directory):
+        with working_dir(self.build_directory):
             make("test")
